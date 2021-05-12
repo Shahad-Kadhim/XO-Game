0f9277596ebf8a3954db05c2ff551478d18ed725
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-05-12 17:30:18 +0300

    initial commit

---
+++ app/src/main/java/com/example/xogame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.xogame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
